b1e6d5d40ffa663fea109f0292d39c321174859b
PotcFdk <PotcFdk@gmx.net>
2017-12-08 00:59:28 +0100

    Add example files.

---
+++ lua/chatsounds/lists_extras/donk.lua
@@ -0,0 +1,4 @@
+c.StartList("donk")
+L["you know what you wanna do with that right"]={{path="chatsounds/example/donk/you know what you wanna do with that right.ogg",length=1.343}}
+L["you wanna put a banging donk on it"]={{path="chatsounds/example/donk/you wanna put a banging donk on it.ogg",length=1.732}}
+c.EndList()
